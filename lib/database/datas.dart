beb762c3f383ac4b292aa3fa48a25c00fd45469e
RyoyaKurihara <t.4178.nxivn47@gmail.com>
2023-11-18 21:25:00 +0900

    select, insert, deleteの追加

--- lib/database/datas.dart
+++ lib/database/datas.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
 
 part 'datas.g.dart';
 
@@ -11,9 +16,30 @@ class TableName extends Table {
 
 @DriftDatabase(tables: [TableName])
 class DatabaseClassName extends _$DatabaseClassName {
-  DatabaseClassName(super.e);
+  DatabaseClassName(): super(_openConnection());
 
   @override
   // TODO: implement schemaVersion
-  int get schemaVersion => throw UnimplementedError();
+  int get schemaVersion => 1;
+
+}
+
+Stream<List<RecordsClass>> watchAllData( DatabaseClassName database ){
+  return database.select(database.tableName).watch();
+}
+
+Future insertData( DatabaseClassName database, RecordsClass record ){
+  return database.into( database.tableName ).insert(record);
+}
+
+Future deleteData( DatabaseClassName database, RecordsClass record ){
+  return database.delete( database.tableName ).delete(record);
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'dates.sqlite'));
+    return NativeDatabase(file);
+  });
 }
